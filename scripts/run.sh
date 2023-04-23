f1b0bd63bfc94483e076787ebbe2c036fcb0f360
Mads Bram Cordes <Mobilpadde@gmail.com>
2023-04-23 12:45:31 +0200

    🐛 fix(run.sh): change database restore condition to if-db-not-exists ✨ feat(run.sh): add support for if-db-not-exists flag in database restore The script now uses the if-db-not-exists flag in the database restore command, which ensures that the database is only restored if it does not already exist. This improves the script's reliability and prevents unnecessary database restores.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,9 @@
 set -e
 
 # Restore the database if it does not already exist
-if [ -f /data/db ]; then
-  echo "Database already exists, skipping restore"
-else
-  echo "No database found, restoring from replica if exists"
-  litestream restore -v -if-replica-exists -o /data/db "${REPLICA_URL}"
-fi
+litestream restore -v -if-db-not-exists -if-replica-exists -o /data/db $REPLICA_URL
 
-# Run litestream with your Pocketbase as the sub-process
+# Run litestream with Pocketbase as it's sub-process
 exec litestream replicate -exec "pocketbase serve --dir /data --http 0.0.0.0:8090"
 
 # Heavily inspired by 🤫
